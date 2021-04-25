7027181ae63c6dc68320630dad513850d683be28
senne <senne.rosaer@student.uantwerpen.be>
2021-04-25 16:37:31 +0200

    semi visualisation

--- main.py
+++ main.py
@@ -1,16 +1,63 @@
-# This is a sample Python script.
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as pltc
+def read_data():
+    connections = dict()
+    opinions = dict()
+    with open("reddit_twitter_data/Twitter/edges_twitter.txt", "r") as f:
+        for line in f.readlines():
+            u,v = line.replace("\n",'').split("\t")
+            if u not in connections:
+                connections[u] = []
+            if v not in connections:
+                connections[v] = []
+            connections[u].append(v)
+            connections[v].append(u)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    with open("reddit_twitter_data/Twitter/twitter_opinion.txt", "r") as f:
+        for line in f.readlines():
+            u,v,w = line.replace("\n",'').split("\t")
+            if u not in opinions:
+                opinions[u] = []
+            opinions[u].append(float(w))
+    return connections, opinions
 
+def to_hex(rgba_color):
+    red = int(rgba_color[0])
+    green = int(rgba_color[1])
+    blue = int(rgba_color[2])
+    return '#%02x%02x%02x' % (red, green, blue)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    conn, op = read_data()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    G = nx.Graph()
+    colors = []
+
+    to_sort = []
+
+    for key in conn:
+        mean = np.mean(op[key])
+        other_mean = [np.mean(op[tmp]) for tmp in conn[key]]
+        color = mean*(len(other_mean)+1)
+        for item in other_mean:
+            color -= item
+        color = (np.minimum(np.maximum(color,0),1)*2)-1
+        if color > 0:
+            to_sort.append(((key, {"color": pltc.to_hex([color, 0,0])}), color))
+
+        else:
+            to_sort.append(((key, {"color": pltc.to_hex([0, 0, abs(color)])}), color))
+
+    to_sort.sort(key = lambda x: x[1])
+    pos = dict()
+    for item in to_sort:
+        G.add_node(item[0][0])
+        colors.append(item[0][1]["color"])
+        pos[item[0][0]] = [np.random.rand(), item[1]]
+    for key in conn:
+        for node in conn[key]:
+            G.add_edge(key, node, weight=1)
+    nx.draw(G, node_color=colors, pos=pos)
+    plt.show()
